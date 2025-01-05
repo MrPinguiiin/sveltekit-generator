1c93912c3bb0d31e842b5a70896db9b07ba9059e
Mr Pinguiiin <52125490+MrPinguiiin@users.noreply.github.com>
2025-01-05 21:41:17 +0700

    fix: ExperimentalWarning

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { makeRoute, makeComponent } from './commands/makeRoute';
 const program = new Command();
 
 program
-  .version('1.1.3')
+  .version('1.1.4')
   .description('A CLI tool to generate SvelteKit routes, components, and server functions');
 
 // Command untuk membuat route
